d40cda1201cc123737a2eb92d5054efab75f5125
Leonardo Martinez <leonardo.martinezmarco@gmail.com>
2020-06-05 18:55:44 -0300

    Exercícios de Angular I

--- typescript/exercicios-typescript II/ex3.ts
+++
@@ -1,9 +0,0 @@
-import * as rs from 'readline-sync';
-
-let anoBissexto = Number(rs.question('Digite um ano:'));
-
-if (anoBissexto % 4 == 0 && anoBissexto % 100 != 0 || anoBissexto % 400 == 0) {
-    console.log("Este ano é bissexto")
-} else {
-    console.log("Este ano não é bissexto")
-}
--- typescript/exercicios-typescript II/ex8.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var rs = require("readline-sync");
-var resultado = 0;
-for (var i = 1; i <= 10; i++) {
-    var n = Number(rs.question("Digite um numero " + i + ": "));
-    resultado += n;
-}
-console.log("A soma de todos os numeros digitados é: " + resultado);
